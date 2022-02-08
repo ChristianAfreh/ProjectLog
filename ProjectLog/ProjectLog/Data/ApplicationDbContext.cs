a904cd6fbdd89466b723f0a69197896a565335f6
Christian Afreh <afrehchristian@gmail.com>
2022-02-08 09:49:46 +0000

    Created an asp.net core 5 project

---
+++ ProjectLog/ProjectLog/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectLog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
